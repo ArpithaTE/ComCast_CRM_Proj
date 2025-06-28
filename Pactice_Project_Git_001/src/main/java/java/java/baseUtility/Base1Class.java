090c2502f9a5ef9eb1c74aa8cb4850dc105c311f
ArpithaTE <arpitha.rpea29@gmail.com>
2025-06-28 15:36:28 +0530

    Ravi shanka

--- Pactice_Project_Git_001/src/main/java/java/java/baseUtility/Base1Class.java
+++ Pactice_Project_Git_001/src/main/java/java/java/baseUtility/Base1Class.java
@@ -3,5 +3,8 @@ package java.java.baseUtility;
 public class Base1Class {
 public static void main(String[] args) {
 	System.out.println("Base1Class");
+	System.out.println("This is the base class for utility functions.");
+	System.out.println("It contains common methods that can be used across different projects.");
+	System.out.println("You can extend this class to create more specific utility classes.");
 }
 }
